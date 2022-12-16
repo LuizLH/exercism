26570fb0b8fa24b1207e300cda457c06268c8c92
LuizLH <luizlh@gmail.com>
2022-12-15 22:43:00 -0300

    feat: implementation of problem

--- ruby/exercises/concept/amusement-park-improvements/attendee.rb
+++ ruby/exercises/concept/amusement-park-improvements/attendee.rb
@@ -1,6 +1,7 @@
 class Attendee
   def initialize(height)
     @height = height
+    @pass_id = nil
   end
 
   def issue_pass!(pass_id)
@@ -14,14 +15,22 @@ class Attendee
   # Do not edit above methods, add your own methods below.
 
   def has_pass?
-    raise 'Please implement the Attendee#haspass? method'
+    if @pass_id == nil
+      false
+    else
+      true
+    end
   end
 
   def fits_ride?(ride_minimum_height)
-    raise 'Please implement the Attendee#fits_ride? method'
+    if @height >= ride_minimum_height
+      true
+    else
+      false
+    end
   end
 
   def allowed_to_ride?(ride_minimum_height)
-    raise 'Please implement the Attendee#allowed_to_ride? method'
+    fits_ride?(ride_minimum_height) and has_pass?
   end
 end
