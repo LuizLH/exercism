7f02da104064272f55d7d6fef2e5d112aa486846
LuizLH <luizlh@gmail.com>
2022-12-15 23:27:45 -0300

    feat: implementation of problem

--- ruby/exercises/concept/assembly-line/assembly_line.rb
+++ ruby/exercises/concept/assembly-line/assembly_line.rb
@@ -1,13 +1,27 @@
+# table shows how speed influences the success rate:
+# 1 to 4: 100% success rate.
+# 5 to 8: 90% success rate.
+# 9: 80% success rate.
+# 10: 77% success rate.
 class AssemblyLine
   def initialize(speed)
     @speed = speed
   end
 
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    total = @speed * 221
+
+    case @speed
+    when 1..4 then (total * 100) / 100.to_f
+    when 5..8 then (total * 90) / 100.to_f
+    when 9 then (total * 80) / 100.to_f
+    when 10 then (total * 77) / 100.to_f
+    else 0
+    end
   end
 
+  # calculate how many completed, working cars are produced per minute.
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    (production_rate_per_hour / 60).to_i
   end
 end
