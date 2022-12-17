6ebc658a547950147b0fa92d9749340ea364685f
LuizLH <luizlh@gmail.com>
2022-12-16 22:28:28 -0300

    feat: implementation of problem

--- ruby/exercises/concept/savings-account/savings_account.rb
+++ ruby/exercises/concept/savings-account/savings_account.rb
@@ -1,13 +1,51 @@
+# SavingsAccount
 module SavingsAccount
   def self.interest_rate(balance)
-    raise 'Please implement the SavingsAccount.interest_rate method'
+    vlr = if balance.instance_of?(String)
+            balance.gsub(/_/, '')
+          else
+            balance
+          end
+
+    if vlr.negative?
+      3.213
+    elsif !vlr.negative? && (vlr.to_f < 1000.00)
+      0.5
+    elsif vlr >= 1000.00 && vlr < 5000.00
+      1.621
+    elsif vlr >= 5000.00
+      2.475
+    end
   end
 
   def self.annual_balance_update(balance)
-    raise 'Please implement the SavingsAccount.annual_balance_update method'
+    vlr = if balance.instance_of?(String)
+      balance.gsub(/_/, '')
+    else
+      balance
+    end
+
+    ((interest_rate(vlr) / 100) + 1) * vlr
   end
 
   def self.years_before_desired_balance(current_balance, desired_balance)
-    raise 'Please implement the SavingsAccount.years_before_desired_balance method'
+    cur_vlr = if current_balance.instance_of?(String)
+                current_balance.gsub(/_/, '')
+              else
+                current_balance
+              end
+
+    des_vlr = if desired_balance.instance_of?(String)
+                desired_balance.gsub(/_/, '')
+              else
+                desired_balance
+              end
+
+    ct = 0
+    while cur_vlr < des_vlr
+      ct += 1
+      cur_vlr = annual_balance_update(cur_vlr)
+    end
+    ct
   end
 end
