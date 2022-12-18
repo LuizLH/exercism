62ad22960b0946e95bb3812335c96b4d6d4a611e
LuizLH <luizlh@gmail.com>
2022-12-17 22:56:02 -0300

    feat: implementation of problem Closes #1

--- ruby/exercises/concept/boutique-inventory/boutique_inventory.rb
+++ ruby/exercises/concept/boutique-inventory/boutique_inventory.rb
@@ -1,28 +1,32 @@
+# BoutiqueInventory
 class BoutiqueInventory
   def initialize(items)
     @items = items
   end
 
   def item_names
-    raise 'Implement the BoutiqueInventory#item_names method'
+    @items.map { |item| item[:name] }.sort
   end
 
   def cheap
-    raise 'Implement the BoutiqueInventory#cheap method'
+    @items.select { |item| item[:price] < 30 }
   end
 
   def out_of_stock
-    raise 'Implement the BoutiqueInventory#out_of_stock method'
+    @items.select { |item| item[:quantity_by_size].empty? }
   end
 
   def stock_for_item(name)
-    raise 'Implement the BoutiqueInventory#stock_for_item method'
+    it = @items.find { |item| item[:name] == name }
+    it[:quantity_by_size] if it
   end
 
   def total_stock
-    raise 'Implement the BoutiqueInventory#total_stock method'
+    @items.sum { |item| item[:quantity_by_size].values.sum }
   end
 
   private
+
   attr_reader :items
+
 end
