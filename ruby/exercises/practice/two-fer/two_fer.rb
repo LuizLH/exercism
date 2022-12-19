f46a73e423f1cbc3a7a9a7919bccdc812d4e3015
LuizLH <luizlh@gmail.com>
2022-12-18 23:56:17 -0300

    feat: implementation of problem Closes #3

--- ruby/exercises/practice/two-fer/two_fer.rb
+++ ruby/exercises/practice/two-fer/two_fer.rb
@@ -1,4 +1,11 @@
-=begin
-Write your code for the 'Two Fer' exercise in this file. Make the tests in
-`two_fer_test.rb` pass.
-=end
+# TwoFer
+class TwoFer
+  def self.two_fer(name = nil)
+
+    if name.nil?
+      'One for you, one for me.'
+    else
+      "One for #{name}, one for me."
+    end
+  end
+end
