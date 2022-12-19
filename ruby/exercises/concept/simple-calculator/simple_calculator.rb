a36a4a42a081c90a02ee196f78f0ca7f1fcb560d
LuizLH <luizlh@gmail.com>
2022-12-18 23:37:59 -0300

    feat: implementation of problem Closes #2

--- ruby/exercises/concept/simple-calculator/simple_calculator.rb
+++ ruby/exercises/concept/simple-calculator/simple_calculator.rb
@@ -1,7 +1,29 @@
 class SimpleCalculator
   ALLOWED_OPERATIONS = ['+', '/', '*'].freeze
 
+  class UnsupportedOperation < StandardError
+    def initialize(msg = 'Unsupported operation')
+      super
+    end
+  end
+
   def self.calculate(first_operand, second_operand, operation)
-    raise 'Please implement the SimpleCalculator.calculate method'
+
+    raise SimpleCalculator::UnsupportedOperation unless ALLOWED_OPERATIONS.include?(operation)
+    raise ArgumentError unless first_operand.is_a?(Integer) && second_operand.is_a?(Integer)
+    raise ZeroDivisionError.new('Division by zero is not allowed.') if operation == '/' && second_operand.zero?
+
+    result_oper = case operation
+                  when '+'
+                    first_operand + second_operand
+                  when '*'
+                    first_operand * second_operand
+                  else
+                    first_operand / second_operand
+                  end
+
+    "#{first_operand} #{operation} #{second_operand} = #{result_oper}"
+  rescue ZeroDivisionError => e
+    e.message
   end
 end
