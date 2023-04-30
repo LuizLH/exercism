ca1803c667cbd227d42aa38877bd6166b15ea947
LuizLH <luizlh@gmail.com>
2023-04-29 23:13:26 -0300

    feat: implementation of problem Closes #8

--- ruby/exercises/practice/word-count/word_count.rb
+++ ruby/exercises/practice/word-count/word_count.rb
@@ -5,3 +5,53 @@ Write your code for the 'Word Count' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/word-count` directory.
 =end
+# ResistorColor
+class Phrase
+
+    def initialize(phrase)
+        @phrase = phrase.downcase
+    end    
+
+    def limpa(st, sp)
+      r = ''
+      st.each_char do |s|
+        if sp.include?(s)
+          r += ' '
+        else
+          r += s
+        end
+      end
+      r
+    end
+  
+    def word_count()
+      words = limpa(@phrase, ',!.&@$%:^')
+
+      words = words.split
+
+      h = Hash.new
+
+      words.each do |s|
+
+        s2 = s.strip
+        if (s2[0] == "'") 
+          s2[0] = ' '
+        end
+        if (s2[-1] == "'")
+          s2[-1] = ' '
+        end
+        r = s2.strip
+        
+        if r != ""
+          if h.include?(r)
+            h[r] += 1
+          else
+            h[r] = 1
+          end
+        end  
+      end
+
+      h
+
+    end
+end
