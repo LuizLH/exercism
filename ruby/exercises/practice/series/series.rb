a0c74809ca0a8383e38565ec1ebee18a02ed3e8a
LuizLH <luizlh@gmail.com>
2023-05-01 23:42:57 -0300

    feat: implementation of problem

--- ruby/exercises/practice/series/series.rb
+++ ruby/exercises/practice/series/series.rb
@@ -1,7 +1,28 @@
-=begin
-Write your code for the 'Series' exercise in this file. Make the tests in
-`series_test.rb` pass.
+class Series
 
-To get started with TDD, see the `README.md` file in your
-`ruby/series` directory.
-=end
+  def initialize(s)
+    @serie = s
+    @r = Array.new
+  end    
+
+  def acumula(n, len)
+    for i in 0..(@r.length - 1) do
+      if @r[i].length < len 
+        @r[i] += n
+      end
+    end
+    @r.push(n)
+  end
+ 
+  def slices(len)
+    raise ArgumentError.new("Slice out of range") unless len <= @serie.length
+    raise ArgumentError.new("Slice out of range") if len <= 0
+    raise ArgumentError.new("Slice out of range") if @serie.empty?
+
+    @serie.chars.each do |n, index|
+      acumula(n, len)
+    end
+    @r.select{ |s| s.length == len }
+  end
+
+end
