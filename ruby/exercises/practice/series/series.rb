99f503684dfe38427117a513ccd05016a9cc229a
LuizLH <luizlh@gmail.com>
2023-05-01 23:52:08 -0300

    feat: improvement implementation

--- ruby/exercises/practice/series/series.rb
+++ ruby/exercises/practice/series/series.rb
@@ -5,22 +5,15 @@ class Series
     @r = Array.new
   end    
 
-  def acumula(n, len)
-    for i in 0..(@r.length - 1) do
-      if @r[i].length < len 
-        @r[i] += n
-      end
-    end
-    @r.push(n)
-  end
- 
   def slices(len)
     raise ArgumentError.new("Slice out of range") unless len <= @serie.length
     raise ArgumentError.new("Slice out of range") if len <= 0
     raise ArgumentError.new("Slice out of range") if @serie.empty?
 
-    @serie.chars.each do |n, index|
-      acumula(n, len)
+    @serie.chars.each do |n|
+        @r = @r.map{ |s| (s.length < len) ? s += n : s}
+
+        @r.push(n)
     end
     @r.select{ |s| s.length == len }
   end
