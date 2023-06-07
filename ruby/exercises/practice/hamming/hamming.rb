d71bd22ee1e77762799c4d13d0abc7cfb551d4ee
LuizLH <luizlh@gmail.com>
2023-06-07 10:26:26 -0300

    feat: correction

--- ruby/exercises/practice/hamming/hamming.rb
+++ ruby/exercises/practice/hamming/hamming.rb
@@ -1,26 +1,23 @@
-=begin
-Write your code for the 'Hamming' exercise in this file. Make the tests in
-`hamming_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/hamming` directory.
-=end
 class Hamming
  
-    def compute(d1, d2)
-        @dna1 = d1.split
-        @dna2 = d2.split
+  def self.compute(d1, d2)
+    raise ArgumentError.new("Strands don't have same length") unless d1.length == d2.length
 
-        raise ArgumentError.new("Strands don't have same length") unless @dna2.length == @dna1.length
+    ct = 0
+    if d1.length > 1
+      @dna1 = d1.chars
+      @dna2 = d2.chars
 
-        ct = 0
-        @dna1.chars.each do |d, idx|
-            if @dna1[idx] != @dna2[idx]
-                ct++
-            end
-        end
-
-        ct
+      @dna1.each_with_index do |d, idx|
+      if d != @dna2[idx]
+        ct = ct + 1
+      end
+      end
+    elsif d1.length == 1
+      if d1 != d2
+        ct = 1
+      end
     end
-
+    ct
+  end
 end
