f8a8e48deba43770f945220457adc734d0e435fe
LuizLH <luizlh@gmail.com>
2023-06-08 15:44:35 -0300

    feat: optimization Closes #11

--- ruby/exercises/practice/scrabble-score/scrabble_score.rb
+++ ruby/exercises/practice/scrabble-score/scrabble_score.rb
@@ -7,21 +7,20 @@
 # J, X                               8
 # Q, Z                               10
 class Scrabble
-  LETTER = {
-    "A" => 1, "E" => 1, "I" => 1, "O" => 1, "U" => 1, "L" => 1, "N" => 1, "R" => 1, "S" => 1, "T" => 1, 
-    "D" => 2, "G" => 2, 
-    "B" => 3, "C" => 3, "M" => 3, "P" => 3,
-    "F" => 4, "H" => 4, "V" => 4, "W" => 4, "Y" => 4,
-    "K" => 5,
-    "J" => 8, "X" => 8, "Q" => 10, "Z" => 10
-  }.freeze
-
-  def initialize(input)
-    @input = input
+    LETTER_SCORES = {
+      "A" => 1, "E" => 1, "I" => 1, "O" => 1, "U" => 1, "L" => 1, "N" => 1, "R" => 1, "S" => 1, "T" => 1,
+      "D" => 2, "G" => 2,
+      "B" => 3, "C" => 3, "M" => 3, "P" => 3,
+      "F" => 4, "H" => 4, "V" => 4, "W" => 4, "Y" => 4,
+      "K" => 5,
+      "J" => 8, "X" => 8, "Q" => 10, "Z" => 10
+    }.freeze
+    
+    def initialize(input)
+      @input = input.upcase
+    end
+    
+    def score
+      @input.chars.sum { |char| LETTER_SCORES[char] || 0 }
+    end
   end
-
-  def score
-    @input.chars.reduce(0) { |acc, e| acc + LETTER[e.upcase] }
-  end
-
-end
