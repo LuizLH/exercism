45c5e9f84d701e94231b4d40f3ed30819bba2bfd
LuizLH <luizlh@gmail.com>
2023-06-07 22:55:58 -0300

    feat: implementing #9

--- ruby/exercises/practice/raindrops/raindrops.rb
+++ ruby/exercises/practice/raindrops/raindrops.rb
@@ -1,7 +1,19 @@
-=begin
-Write your code for the 'Raindrops' exercise in this file. Make the tests in
-`raindrops_test.rb` pass.
+#has 3 as a factor, add 'Pling' to the result.
+#has 5 as a factor, add 'Plang' to the result.
+#has 7 as a factor, add 'Plong' to the result.
+#does not have any of 3, 5, or 7 as a factor, the result should be the digits of the number.
+class Raindrops
+  def self.convert(n)
 
-To get started with TDD, see the `README.md` file in your
-`ruby/raindrops` directory.
-=end
+    result = ""
+    result += "Pling" if n % 3 == 0 
+    result += "Plang" if n % 5 == 0
+    result += "Plong" if n % 7 == 0
+
+    if result.empty?
+      result = n.to_s
+    end
+
+    result
+  end
+end
