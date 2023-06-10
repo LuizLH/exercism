f6d87a68873e98436fe5e56e8012f4e35d0bdd2e
LuizLH <luizlh@gmail.com>
2023-06-09 23:50:10 -0300

    feat: implemented Closes #12

--- ruby/exercises/practice/luhn/luhn.rb
+++ ruby/exercises/practice/luhn/luhn.rb
@@ -1,3 +1,11 @@
+# Exemple
+# 4539 3195 0343 6467
+# 4_3_ 3_9_ 0_4_ 6_6_
+# 8569 6195 0383 3437
+#
+# 8+5+6+9+6+1+9+5+0+3+8+3+3+4+3+7 = 80
+#
+# If the sum is evenly divisible by 10, then the number is valid. This number is valid!
 class Luhn
  
     def valid?(number)
