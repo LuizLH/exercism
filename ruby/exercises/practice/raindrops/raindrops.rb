4ce76bec3f8e08c5455703ee3f99e739eb4e534e
LuizLH <luizlh@gmail.com>
2023-06-09 23:46:23 -0300

    feat: optimization Closes #9

--- ruby/exercises/practice/raindrops/raindrops.rb
+++ ruby/exercises/practice/raindrops/raindrops.rb
@@ -2,6 +2,7 @@
 #has 5 as a factor, add 'Plang' to the result.
 #has 7 as a factor, add 'Plong' to the result.
 #does not have any of 3, 5, or 7 as a factor, the result should be the digits of the number.
+
 class Raindrops
   def self.convert(n)
     result = String.new
