8b3315db5bbd62a26bf7e3d181cbea01a88e4d3a
LuizLH <luizlh@gmail.com>
2023-06-10 23:45:50 -0300

    feat: implemented

--- ruby/exercises/practice/clock/clock.rb
+++ ruby/exercises/practice/clock/clock.rb
@@ -1,11 +1,12 @@
 # Implement a clock that handles times without dates.
 # You should be able to add and subtract minutes to it.
 # Two clocks that represent the same time should be equal to each other.
+
 class Luhn
  
   def initialize()
 
-    
+
   end
   
 end
